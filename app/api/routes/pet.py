0c3047e88ecd1777f30e7b500340445363730eb9
Jon Kevin Goncalves Hurtado <jonkevin.goncalves@servosa.com>
2025-03-29 12:37:12 +0100

    Added railway.json

--- app/api/routes/pet.py
+++ app/api/routes/pet.py
@@ -6,22 +6,42 @@ from app.models.assign_pet_request import AssignPetRequest
 
 router = APIRouter()
 
-@router.post("/create")
+@router.post("/create", summary="Crear una nueva mascota", description="Este endpoint crea una nueva mascota en el sistema", response_model=Pet)
 def create(pet: Pet):
+    """
+    Crea una nueva mascota.
+    - **pet**: Información de la mascota a crear.
+    """
     return create_pet(pet)
 
-@router.get("/get/{pet_id}")
+@router.get("/get/{pet_id}", summary="Obtener información de una mascota", description="Este endpoint obtiene la información de una mascota usando su ID", response_model=Pet)
 def get(pet_id: str):
+    """
+    Obtiene la información de una mascota a través de su ID.
+    - **pet_id**: El ID único de la mascota.
+    """
     return get_pet(pet_id)
 
-@router.post("/assign_pet")
+@router.post("/assign_pet", summary="Asignar una mascota a un dueño", description="Este endpoint asigna una mascota a un dueño", response_model=Pet)
 def assign(request: AssignPetRequest):
+    """
+    Asigna una mascota a un dueño.
+    - **request**: Información sobre la asignación (mascota y dueño).
+    """
     return assign_pet(request)
 
-@router.post("/validate_qr")
+@router.post("/validate_qr", summary="Validar un código QR", description="Este endpoint valida un código QR para obtener información relacionada con una mascota.")
 def validate_qr_code(content: str):
+    """
+    Valida un código QR.
+    - **content**: El contenido del código QR que se desea validar.
+    """
     return validate_qr(content)
 
-@router.delete("/delete/{pet_id}")
+@router.delete("/delete/{pet_id}", summary="Eliminar una mascota", description="Este endpoint elimina una mascota del sistema usando su ID")
 def delete(pet_id: str):
+    """
+    Elimina una mascota por su ID.
+    - **pet_id**: El ID único de la mascota a eliminar.
+    """
     return delete_pet(pet_id)
