1ba0e3d53149edd9f97f942a13cabbb16c4e6594
Jon Goncalves <153616095+zjonkg@users.noreply.github.com>
2025-04-04 16:31:14 +0200

    Uploaded config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,18 +1,15 @@
 import os
-from dotenv import load_dotenv
 import asyncpg
-
-# Cargar variables desde el archivo .env
-load_dotenv()
 
 # Obtener las credenciales
 SUPABASE_URL = os.getenv("SUPABASE_URL")
-async def connect_db():
-    return await asyncpg.connect(SUPABASE_URL)
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
 # Verificar si las credenciales están correctamente cargadas
 if not SUPABASE_URL or not SUPABASE_KEY:
-    raise ValueError("❌ Error: Las credenciales de Supabase no están configuradas correctamente en .env")
+    raise ValueError("❌ Error: Las credenciales de Supabase no están configuradas correctamente en Railway")
 
 print("✅ Variables de entorno cargadas correctamente.")
+
+async def connect_db():
+    return await asyncpg.connect(SUPABASE_URL)
