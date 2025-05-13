844552c9f112395360f7fc8b060e1fbec3f67400
C3rb3rux-Code <polcrepal@campus.monlau.com>
2025-05-13 16:31:27 +0200

    employees fix

--- app/models/employees.py
+++ app/models/employees.py
@@ -12,7 +12,7 @@ class Employee(BaseModel):
 class EmployeeAll(Employee):
     second_name: str
     second_surname: str
-    birthday: str
+    birthdate: str
     address: str
 
 class LoginRequest(BaseModel):
